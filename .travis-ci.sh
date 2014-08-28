ad5f0c325eff767c5e23397d05611fd19a98a145
Thomas Gazagnaire <thomas@gazagnaire.org>
2014-08-28 10:57:26 +0100

    Drop testing for 4.01.0

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,10 +1,9 @@
 # Install OCaml and OPAM PPAs
-case "$OCAML_VERSION,$OPAM_VERSION" in
-  4.00.1,1.1.0) ppa=avsm/ocaml40+opam11 ;;
-  4.01.0,1.1.0) ppa=avsm/ocaml41+opam11 ;;
-  4.02.0,1.1.0) ppa=avsm/ocaml41+opam11 ;;
-  4.02.0+doc,1.1.0) ppa=avsm/ocaml41+opam11 ;;
-  *) echo Unknown $OCAML_VERSION,$OPAM_VERSION; exit 1 ;;
+case "$OCAML_VERSION" in
+  4.01.0) ppa=avsm/ocaml41+opam11 ;;
+  4.02.0) ppa=avsm/ocaml41+opam11 ;;
+  4.02.0+doc) ppa=avsm/ocaml41+opam11 ;;
+  *) echo Unknown $OCAML_VERSION; exit 1 ;;
 esac
 
 echo "yes" | sudo add-apt-repository ppa:$ppa
@@ -20,12 +19,15 @@ opam init git://github.com/ocaml/opam-repository >/dev/null 2>&1
 opam repo add platform .
 
 case "$OCAML_VERSION" in
+  4.01.0)
+    opam install ocaml-org ;;
   4.02.0)
-    opam switch 4.02.0+rc1 ;;
+    opam switch 4.02.0+rc1
+    opam install ocaml-org ;;
   4.02.0+doc)
     opam switch 4.02.0+doc
     opam install opam-doc-base ;;
-  *) echo "Skipping installation of opam-doc-base" ;;
+  *)
+    echo "Nothing to do";
+    exit 1 ;;
 esac
-
-opam install ocaml-org
