ba47799c287eda68f910e8578282a15e8bdb9099
David Sheets <sheets@alum.mit.edu>
2015-01-14 09:56:19 +0000

    Correct Travis PPA name

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,8 +1,8 @@
 # Install OCaml and OPAM PPAs
 case "$OCAML_VERSION" in
   4.01.0) ppa=avsm/ocaml41+opam12 ;;
-  4.02.1) ppa=avsm/ocaml41+opam12 ;;
-  4.02.1+doc) ppa=avsm/ocaml41+opam12 ;;
+  4.02.1) ppa=avsm/ocaml42+opam12 ;;
+  4.02.1+doc) ppa=avsm/ocaml42+opam12 ;;
   *) echo Unknown $OCAML_VERSION; exit 1 ;;
 esac
 
